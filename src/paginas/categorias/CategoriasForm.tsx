a051ac2c336a5511ebe0507646c338e2870d884e
Franco-videla <fraancovidelaa@gmail.com>
2024-07-28 21:06:17 -0300

    correcion en categoria y unidad de medida

--- src/paginas/categorias/CategoriasForm.tsx
+++ src/paginas/categorias/CategoriasForm.tsx
@@ -4,14 +4,14 @@ import CategoriaService from "../../servicios/CategoriaService";
 import { useAtributos } from "../../hooks/useAtributos";
 import './categorias.css';
 import { useSucursales } from "../../hooks/useSucursales";
-
+import { Button } from "@mui/material";
 function CategoriasForm() {
     const [categoriasSinFiltrar, setCategoriasSinFilrar] = useState<Categoria[]>([]);
     const [mostrarVisibles] = useState<boolean>(true);
     const [txtDenominacionNueva, setTxtDenominacionNueva] = useState<string>("");
     const [txtValidacion, setTxtValidacion] = useState<string>("");
-    const {sucursales, sucursalSeleccionada} = useSucursales();
-    const {categorias, setCategorias} = useAtributos();
+    const { sucursales, sucursalSeleccionada } = useSucursales();
+    const { categorias, setCategorias } = useAtributos();
 
     const urlapi = import.meta.env.VITE_API_URL;
     const categoriaService = new CategoriaService(urlapi + "/api/categorias");
@@ -25,112 +25,131 @@ function CategoriasForm() {
         const categoriasFiltradas: Categoria[] = categoriasSinFiltrar.filter(c => {
             const incluyeSucursal = c.sucursales?.some(s => s.id === sucursalSeleccionada.id);
             return mostrarVisibles ? incluyeSucursal : !incluyeSucursal;
-        }).sort((a,b) => a.denominacion < b.denominacion ? -1 : 1);
+        }).sort((a, b) => a.denominacion < b.denominacion ? -1 : 1);
         setCategorias(categoriasFiltradas);
     }
-    
+
     const changeSucursalSubcategoria = (categoria: Categoria, eliminar: boolean) => {
         categoria.subCategorias.forEach(subcategoria => changeSucursalSubcategoria(subcategoria, eliminar));
         categoria.sucursales = eliminar
             ? categoria.sucursales?.filter(s => s.id !== sucursalSeleccionada.id) || []
             : [...(categoria.sucursales || []), sucursalSeleccionada];
     }
-    
+
     const actualizarCategoria = async (categoria: Categoria) => {
         await categoriaService.put(categoria.id, categoria);
         await getCategoriasRest();
     };
-    
+
     const changeSucursalPadres = async (categoria: Categoria, eliminar: boolean) => {
         const categoriaPadre = categorias.find(c => c.subCategorias.some(sub => sub.id === categoria.id));
-    
+
         if (eliminar || !categoriaPadre) {
             await actualizarCategoria(categoria);
             return;
         }
-    
+
         categoriaPadre.sucursales = [
             ...categoria.sucursales?.filter(s => s.id !== sucursalSeleccionada.id) || [],
             sucursalSeleccionada
         ];
-    
+
         categoriaPadre.subCategorias = [
             ...categoriaPadre.subCategorias.filter(sub => sub.id !== categoria.id),
             categoria
         ];
-    
+
         await changeSucursalPadres(categoriaPadre, eliminar);
     };
-    
+
     const handleEliminarEnSucursal = async (categoria: Categoria) => {
         const eliminar = categoria.sucursales?.some(s => s.id === sucursalSeleccionada.id) || false;
         changeSucursalSubcategoria(categoria, eliminar);
         await changeSucursalPadres(categoria, eliminar);
-    };    
+    };
 
-    const mostrarCategorias = (categoria:Categoria, padreId:number) => {
-        const tieneHijos:boolean = categoria.subCategorias.filter(sc => !mostrarVisibles || sc.sucursales?.map(s => s.id).includes(sucursalSeleccionada.id)).length > 0;
+    const mostrarCategorias = (categoria: Categoria, padreId: number) => {
+        const tieneHijos: boolean = categoria.subCategorias.filter(sc => !mostrarVisibles || sc.sucursales?.map(s => s.id).includes(sucursalSeleccionada.id)).length > 0;
         return (
             <div key={categoria.id} className="accordion-item">
-                <h2 className="accordion-header d-flex flex-row" id={"flush-heading"+categoria.id}>
-                {tieneHijos 
-                    ? <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#flush-collapse"+categoria.id} aria-expanded="false" aria-controls={"flush-collapse"+categoria.id}> {categoria.denominacion} </button>
-                    : <div className="accordion-button accordion-no-arrow collapsed"> {categoria.denominacion} </div>
-                }
-                
-                {mostrarVisibles &&
-                <button className="btn mx-2 p-0" type="button" onClick={() => mostrarInput(categoria.id)} >
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#333333" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                        </svg>
-                    </div>
-                </button>
-                }
-                
-                <button className="btn mx-2 p-0" type="button" onClick={() => handleEliminarEnSucursal(categoria)} > 
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        {mostrarVisibles 
-                            ?   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FF0000" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
-                                </svg>
-                            : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00AA00" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                                </svg>
-                        }
-                    </div>
-                </button>
+                <h2 className="accordion-header d-flex flex-row" id={"flush-heading" + categoria.id}>
+                    {tieneHijos
+                        ? <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#flush-collapse" + categoria.id} aria-expanded="false" aria-controls={"flush-collapse" + categoria.id}> {categoria.denominacion} </button>
+                        : <div className="accordion-button accordion-no-arrow collapsed"> {categoria.denominacion} </div>
+                    }
+
+                    {mostrarVisibles &&
+                        <Button
+                            onClick={() => mostrarInput(categoria.id)}
+                            sx={{
+                                bgcolor: "#a6c732",
+                                "&:hover": {
+                                    bgcolor: "#a0b750",
+                                },
+                                my: 3,
+                                mx: 1,
+                                width: "auto",
+                                maxWidth: 200,
+                            }}
+                            variant="contained"
+                        >
+                            Crear
+                        </Button>
+                    }
+
+
+                    <Button
+                        onClick={() => handleEliminarEnSucursal(categoria)}
+                        sx={{
+                            bgcolor: "#e05151",
+                            "&:hover": {
+                                bgcolor: "#a0b750",
+                            },
+                            my: 3,
+                            mx: 1,
+                            width: "auto",
+                            maxWidth: 200,
+                        }}
+                        variant="contained"
+                    >
+                        Eliminar
+                    </Button>
 
                 </h2>
-                
-                <h2 className="accordion-header input-categoria" id={"inputCategoria"+categoria.id} hidden>
+
+                <h2 className="accordion-header input-categoria" id={"inputCategoria" + categoria.id} hidden>
                     <div className="d-flex flex-row">
 
-                    <div className="accordion-button accordion-no-arrow collapsed">
-                        <input type="text" className="form-control" placeholder="Nombre de la categoría" value={txtDenominacionNueva} onChange={(e) => setTxtDenominacionNueva(String(e.target.value))}></input>
-                    </div>
-
-                    <button className="btn mx-2 p-0" type="button" onClick={() => save(categoria.id)}>
-                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00AA00" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                            <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
-                            </svg>
+                        <div className="accordion-button accordion-no-arrow collapsed">
+                            <input type="text" className="form-control" placeholder="Nombre de la categoría" value={txtDenominacionNueva} onChange={(e) => setTxtDenominacionNueva(String(e.target.value))}></input>
                         </div>
-                    </button>
+
+                        <Button
+                            onClick={() => save(categoria.id)}
+                            sx={{
+                                bgcolor: "#a6c732",
+                                "&:hover": {
+                                    bgcolor: "#a0b750",
+                                },
+                                my: 3,
+                                mx: 1,
+                                width: "auto",
+                                maxWidth: 200,
+                            }}
+                            variant="contained"
+                        >
+                            Crear
+                        </Button>
 
                     </div>
                 </h2>
 
-                {tieneHijos && 
-                    <div id={"flush-collapse"+categoria.id} className="accordion-collapse collapse" aria-labelledby={"flush-heading"+categoria.id} data-bs-parent={"#accordionFlush" + padreId}>
+                {tieneHijos &&
+                    <div id={"flush-collapse" + categoria.id} className="accordion-collapse collapse" aria-labelledby={"flush-heading" + categoria.id} data-bs-parent={"#accordionFlush" + padreId}>
                         <div className="accordion-body">
-                        {categoria.subCategorias.filter(sc => !mostrarVisibles || sc.sucursales?.map(s => s.id).includes(sucursalSeleccionada.id)).sort((a,b) => a.denominacion < b.denominacion ? -1 : 1).map(subcategoria => 
-                            mostrarCategorias(subcategoria, categoria.id)
-                        )}
+                            {categoria.subCategorias.filter(sc => !mostrarVisibles || sc.sucursales?.map(s => s.id).includes(sucursalSeleccionada.id)).sort((a, b) => a.denominacion < b.denominacion ? -1 : 1).map(subcategoria =>
+                                mostrarCategorias(subcategoria, categoria.id)
+                            )}
                         </div>
                     </div>
                 }
@@ -139,7 +158,7 @@ function CategoriasForm() {
         )
     }
 
-    const mostrarInput = (categoriaPadreId?:number) => {
+    const mostrarInput = (categoriaPadreId?: number) => {
         const inputs = document.getElementsByClassName("input-categoria");
         for (let i = 0; i < inputs.length; i++) {
             const slide = inputs[i] as HTMLElement;
@@ -151,17 +170,17 @@ function CategoriasForm() {
             document.getElementById("inputCategoria" + categoriaPadreId)?.removeAttribute("hidden");
     }
 
-    const save = async (categoriaPadreId:number | null) => {
+    const save = async (categoriaPadreId: number | null) => {
         if (txtDenominacionNueva == undefined || txtDenominacionNueva == "") {
             setTxtValidacion("Ingrese el nombre de la categoría");
             return;
         }
 
-        const categoriaNueva:Categoria = new Categoria();
+        const categoriaNueva: Categoria = new Categoria();
         categoriaNueva.denominacion = txtDenominacionNueva;
 
         if (categoriaPadreId) {
-            const categoriaPadre:Categoria = categorias.filter(categoria => categoria.id === categoriaPadreId)[0];
+            const categoriaPadre: Categoria = categorias.filter(categoria => categoria.id === categoriaPadreId)[0];
             categoriaPadre.subCategorias.push(categoriaNueva);
             await categoriaService.put(categoriaPadreId, categoriaPadre);
         }
@@ -187,42 +206,48 @@ function CategoriasForm() {
 
     return (
         <>
-        <div className="row">
-        <div className="col-sm">
+            <div className="categoria-container">
+                <div className="categoria-header">
+                    <div className="ms-3 mb-2 d-flex flex-row">
+                        <input type="text" className="form-control categoria-input" placeholder="Nombre de la categoría" value={txtDenominacionNueva} onChange={(e) => setTxtDenominacionNueva(String(e.target.value))}></input>
+                        <Button
+                            onClick={() => save(null)}
+                            sx={{
+                                bgcolor: "#a6c732",
+                                "&:hover": {
+                                    bgcolor: "#a0b750",
+                                },
 
-            <div className="ms-3 mb-2 d-flex flex-row">
-                <input type="text" className="form-control" placeholder="Nombre de la categoría" value={txtDenominacionNueva} onChange={(e) => setTxtDenominacionNueva(String(e.target.value))}></input>
-                <button className="btn mx-2 p-0" type="button" onClick={() => save(null)}>
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00AA00" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                        <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
-                        </svg>
+                            }}
+                            variant="contained"
+                        >
+                            Crear
+                        </Button>
+
                     </div>
-                </button>
-                
-            </div>
 
-            <div className="accordion accordion-flush" id="accordionFlush0">
-                {categorias
-                    .filter(
-                        categoria => { return !categorias
-                            .some( categoriaPadre => categoriaPadre.subCategorias
-                                .some( subcategoria => subcategoria.id === categoria.id) 
-                            ); 
+                    <div className="accordion accordion-flush categoria-accordion"
+                        id="accordionFlush0">
+                        {categorias
+                            .filter(
+                                categoria => {
+                                    return !categorias
+                                        .some(categoriaPadre => categoriaPadre.subCategorias
+                                            .some(subcategoria => subcategoria.id === categoria.id)
+                                        );
+                                }
+                            )
+                            .sort((a, b) => (a.denominacion < b.denominacion ? -1 : 1))
+                            .map(categoria => mostrarCategorias(categoria, 0))
                         }
-                    )
-                    .sort((a, b) => (a.denominacion < b.denominacion ? -1 : 1))
-                    .map(categoria => mostrarCategorias(categoria, 0))
-                }
-            </div>
+                    </div>
 
-            <div>
-                <label style={{ color: 'red', lineHeight: 5, padding: 5, userSelect: "none"}}>{txtValidacion}</label>
-            </div>
+                    <div>
+                        <label style={{ color: 'red', lineHeight: 5, padding: 5, userSelect: "none" }}>{txtValidacion}</label>
+                    </div>
 
-        </div>
-        </div>
+                </div>
+            </div>
 
         </>
     );
--- src/paginas/unidadesMedida/UnidadesMedidaForm.tsx
+++ src/paginas/unidadesMedida/UnidadesMedidaForm.tsx
@@ -4,27 +4,29 @@ import UnidadMedidaService from "../../servicios/UnidadMedidaService";
 import UnidadMedida from "../../entidades/UnidadMedida";
 import { useSucursales } from "../../hooks/useSucursales";
 import './unidadesMedidas.css';
-
+import {  Button } from "@mui/material";
+import { cilTrash } from "@coreui/icons";
+import CIcon from "@coreui/icons-react";
 function UnidadesMedidaForm() {
     const [mostrarVisibles] = useState<boolean>(true);
     const [txtDenominacionNueva, setTxtDenominacionNueva] = useState<string>("");
     const [txtValidacion, setTxtValidacion] = useState<string>("");
-    const {sucursales, sucursalSeleccionada} = useSucursales();
-    const {unidadesMedida, setUnidadesMedida} = useAtributos();
+    const { sucursales, sucursalSeleccionada } = useSucursales();
+    const { unidadesMedida, setUnidadesMedida } = useAtributos();
 
     const urlapi = import.meta.env.VITE_API_URL;
     const unidadesMedidaService = new UnidadMedidaService(urlapi + "/api/unidadesmedida");
 
     const getUnidadesMedidaRest = async () => {
-        const nuevasUnidades:UnidadMedida[] = await unidadesMedidaService.getAll();
+        const nuevasUnidades: UnidadMedida[] = await unidadesMedidaService.getAll();
         const unidadesFiltradas: UnidadMedida[] = nuevasUnidades.filter(u => {
             const incluyeSucursal = u.sucursales?.some(s => s.id === sucursalSeleccionada.id);
             return mostrarVisibles ? incluyeSucursal : !incluyeSucursal;
-        }).sort((a,b) => a.denominacion < b.denominacion ? -1 : 1);
+        }).sort((a, b) => a.denominacion < b.denominacion ? -1 : 1);
         setUnidadesMedida(unidadesFiltradas);
     }
 
-    const mostrarInput = (categoriaPadreId?:number) => {
+    const mostrarInput = (categoriaPadreId?: number) => {
         const inputs = document.getElementsByClassName("input-categoria");
         for (let i = 0; i < inputs.length; i++) {
             const slide = inputs[i] as HTMLElement;
@@ -35,7 +37,7 @@ function UnidadesMedidaForm() {
         if (categoriaPadreId !== null)
             document.getElementById("inputCategoria" + categoriaPadreId)?.removeAttribute("hidden");
     }
-    
+
     const handleEliminarEnSucursal = async (unidadMedida: UnidadMedida) => {
         if (unidadMedida.sucursales?.some(s => s.id === sucursalSeleccionada.id)) {
             unidadMedida.sucursales = unidadMedida.sucursales.filter(s => s.id !== sucursalSeleccionada.id);
@@ -44,7 +46,7 @@ function UnidadesMedidaForm() {
         }
         await unidadesMedidaService.put(unidadMedida.id, unidadMedida);
         getUnidadesMedidaRest();
-    }; 
+    };
 
     const save = async () => {
         if (txtDenominacionNueva == undefined || txtDenominacionNueva == "") {
@@ -56,7 +58,7 @@ function UnidadesMedidaForm() {
             return;
         }
 
-        const unidadNueva:UnidadMedida = new UnidadMedida();
+        const unidadNueva: UnidadMedida = new UnidadMedida();
         unidadNueva.denominacion = txtDenominacionNueva;
         unidadNueva.sucursales = sucursales;
         await unidadesMedidaService.post(unidadNueva);
@@ -72,53 +74,65 @@ function UnidadesMedidaForm() {
     }, [mostrarVisibles, sucursalSeleccionada]);
 
     return (
-        <div className="row">
-        <div className="col-sm">
+        <div className="unidad-medida-container">
+            <div className="unidad-medida-header">
+                <div className="ms-3 d-flex flex-row">
+                    <input type="text" className="form-control" placeholder="Unidad de medida" value={txtDenominacionNueva} onChange={(e) => setTxtDenominacionNueva(String(e.target.value))}></input>
+                    <Button
+                    onClick={save}
+                    sx={{
+                        bgcolor: "#a6c732",
+                        "&:hover": {
+                            bgcolor: "#a0b750",
+                        },
+                        my: 1,
+                        mx: 1,
+                        width: '12%',
+                       
+                    }}
+                    variant="contained"
+                >
+                    Crear
+                </Button>
 
-            <div className="ms-3 d-flex flex-row">
-                <input type="text" className="form-control" placeholder="Unidad de medida" value={txtDenominacionNueva} onChange={(e) => setTxtDenominacionNueva(String(e.target.value))}></input>
-                <button className="btn mx-2 p-0" type="button" onClick={save}>
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00AA00" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                        <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
-                        </svg>
-                    </div>
-                </button>
+                </div>
                 
-            </div>
-
-            <div className="accordion accordion-flush">
-                {unidadesMedida.map(unidadMedida => { return (
-                    <div key={unidadMedida.id} className="accordion-item">
-                        <h2 className="accordion-header d-flex flex-row" id={"flush-heading"+unidadMedida.id}>
-                        
-                        <div className="accordion-button accordion-no-arrow collapsed"> {unidadMedida.denominacion} </div>
-                            <button className="btn mx-2 p-0" type="button" onClick={() => handleEliminarEnSucursal(unidadMedida)} > 
-                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                {mostrarVisibles 
-                                    ?   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FF0000" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
-                                        </svg>
-                                    : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00AA00" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                                        </svg>
-                                }
+                <div className="accordion accordion-flush unidad-medida-accordion">
+                    {unidadesMedida.map(unidadMedida => {
+                        return (
+                            <div key={unidadMedida.id} className="accordion-item">
+                            <h2 className="accordion-header d-flex flex-row" id={"flush-heading" + unidadMedida.id}>
+                                <div className="accordion-button custom-accordion-button accordion-no-arrow collapsed"> {unidadMedida.denominacion} </div>
+                                <Button
+                                    style={{
+                                        margin:"10px 1px",
+                                        backgroundColor: "#e05151",
+                                        padding: "10px 15px",     // Ajusta el tamaño del botón
+                                        height: "40px",
+                                        width: "50px",
+                                        minWidth: "auto",        // Asegura que el ancho mínimo sea automático
+                                        minHeight: "10px",       // Asegura que la altura mínima sea automática
+                                        right: '1%'
+                                    }}
+                                    className="custom-button"
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={() => handleEliminarEnSucursal(unidadMedida)}
+                                >
+                                    <CIcon icon={cilTrash} size="sm" />
+                                </Button>
+                                </h2>
                             </div>
-                            </button>
-                        </h2>
-                    </div>
-                )})
-                }
-            </div>
+                        )
+                    })
+                    }
+                </div>
 
-            <div>
-                <label style={{ color: 'red', lineHeight: 5, padding: 5, userSelect: "none"}}>{txtValidacion}</label>
-            </div>
+                <div>
+                    <label style={{ color: 'red', lineHeight: 5, padding: 5, userSelect: "none" }}>{txtValidacion}</label>
+                </div>
 
-        </div>
+            </div>
         </div>
     );
 }
